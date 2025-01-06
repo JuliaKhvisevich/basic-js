b667fa97513fc26b1e43c542eb9deffafe8edc18
JuliaKhvisevich <reytjulia@gmail.com>
2025-01-06 17:28:48 +0200

    feat: add add tasks solving

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const result = [];
+
+  for (let i = 0; i < rows; i++) {
+    result.push(new Array(cols).fill(0));
+  }
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (matrix[row][col] === true) {
+        for (let dRow = -1; dRow <= 1; dRow++) {
+          for (let dCol = -1; dCol <= 1; dCol++) {
+            if (dRow === 0 && dCol === 0) continue;
+
+            const newRow = row + dRow;
+            const newCol = col + dCol;
+
+            if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+              result[newRow][newCol]++;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return result;
 }
+
 
 module.exports = {
   minesweeper
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const positiveNumbers = arr.filter(num => num >= 0);
+
+  positiveNumbers.sort((a, b) => a - b);
+
+  const result = arr.map(num => {
+    if (num < 0) {
+      return num;
+    } else {
+      return positiveNumbers.shift();
+    }
+  });
+
+  return result;
+
 }
 
 module.exports = {
